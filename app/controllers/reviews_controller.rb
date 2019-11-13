2611324422e42db9b00a12fb03b067e55a1571a0
Thibault Monnoyeur <thibault.monnoyeur@gmail.com>
2019-11-13 16:02:45 +0100

    d

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,46 @@
+class ReviewsController < ApplicationController
+  # def index
+  #   @reviews = Review.all
+  # end
+
+  # def show
+  #   @review = Review.find(params[:id])
+  # end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+# def edit
+  #   @review = Review.find(params[:id])
+  # end
+
+  # def update
+  #   @review = Review.find(params[:id])
+  #   @review.update(review_params)
+  # end
+
+  # def destroy
+  #   @review = Review.find(params[:id])
+  #   @review.destroy
+  # end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
